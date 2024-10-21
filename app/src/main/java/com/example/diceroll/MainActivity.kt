fb2e0ab29e49663d1ff32b467839b7102157ed50
Mahez_ <batubattery25@gmail.com>
2024-10-21 10:50:45 +0700

    add: button

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroll.ui.theme.DiceRollTheme
 
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
     DiceWithButtonAndImage()
@@ -44,6 +46,10 @@ fun DiceWithButtonAndImage(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Button(
+            onClick = { /*TODO*/ },
+        ) {
+            Text(stringResource(R.string.roll))
+        }
     }
 }
